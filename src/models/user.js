6f793a29a3526ee5a7fc59bd6705729b60563ae7
MiqueiasGomes06 <miqueiasgomes.2001@gmail.com>
2023-08-20 00:54:56 -0300

    intalando a biblioteca bcryptjs para encripitar as senhas

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -20,6 +21,16 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+})
+
+UserSchema.pre("save", async function (next) {
+  try {
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+    next()
+  } catch (error) {
+    next(error)
+  }
 })
 
 const User = mongoose.model("User", UserSchema)
